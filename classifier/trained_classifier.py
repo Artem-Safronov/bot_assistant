5192de0bbf960d92ba0110ae4c1f90238e90e579
Artem Safronov <a.safronov_job@mail.ru>
2023-07-21 23:39:24 +0300

    обучение модели

---
+++ classifier/trained_classifier.py
@@ -0,0 +1,27 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+import yaml
+
+
+def trained_classifier():
+    with open("classifier/training_data.yml") as file:
+        data = yaml.safe_load(file)
+
+    x_train = []
+    y_train = []
+    for intent in data["intents"]:
+        for example in intent["examples"].split("\n"):
+            train = example.strip(" .,-!?")
+            if train:
+                x_train.append(train)
+                y_train.append(intent["intent"])
+
+    vectorizer = CountVectorizer()
+    training_features = vectorizer.fit_transform(x_train)
+
+    classifier = MultinomialNB()
+    classifier.fit(training_features, y_train)
+
+    with open("classifier/trained_models/trained_model.pkl", "wb") as file:
+        pickle.dump(classifier, file)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import settings
 from aiogram import Bot, Dispatcher, executor, types
+from classifier.trained_classifier import trained_classifier
 
 
 bot = Bot(settings.API_TOKEN)
@@ -12,4 +13,5 @@ async def text(message: types.Message):
 
 
 if __name__ == '__main__':
+    trained_classifier()
     executor.start_polling(dp)
