edd37c74e4809d28add8ada41528eb8981e9c4c5
Artem Safronov <a.safronov_job@mail.ru>
2023-07-22 22:03:50 +0300

    fix

--- integration/http_client.py
+++ integration/http_client.py
@@ -1,7 +1,9 @@
+import json
 import aiohttp
 
 
 async def http_client(url=None, headers=None, params=None):
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url=url, params=params) as response:
-            return await response.text()
+            text = await response.text()
+            return json.loads(text)
