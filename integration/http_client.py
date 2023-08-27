beaadfae8dfae2136c3abc7b7b5221b4340666a7
Artem Safronov <a.safronov_job@mail.ru>
2023-08-27 22:01:15 +0300

    изменил метод

--- integration/http_client.py
+++ integration/http_client.py
@@ -2,8 +2,8 @@ import json
 import aiohttp
 
 
-async def http_client(url=None, headers=None, params=None):
+async def http_client(method="GET", url=None, headers=None, params=None):
     async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url=url, params=params) as response:
+        async with session.request(method=method, url=url, params=params) as response:
             text = await response.text()
             return json.loads(text)
