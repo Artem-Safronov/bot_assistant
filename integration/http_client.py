8006a9ca9131075c5533613cf60ccbd9a0226859
Artem Safronov <a.safronov_job@mail.ru>
2023-08-31 22:59:27 +0300

    обработка ошибки

--- integration/http_client.py
+++ integration/http_client.py
@@ -1,9 +1,20 @@
 import json
-import aiohttp
+from aiohttp import ClientSession, ClientTimeout
+import logging
+import traceback
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def http_client(method="GET", url=None, headers=None, params=None):
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.request(method=method, url=url, params=params) as response:
-            text = await response.text()
-            return json.loads(text)
+    try:
+        async with ClientSession(headers=headers, timeout=ClientTimeout(total=3.0)) as session:
+            async with session.request(method=method, url=url, params=params) as response:
+                text = await response.text()
+                logger.info(f"Response: {text}")
+                return json.loads(text)
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return {}
