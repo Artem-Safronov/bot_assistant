566f95e7930e40059526c24f218766228ad08bc6
Artem Safronov <a.safronov_job@mail.ru>
2023-10-08 18:14:01 +0300

    fix

--- integration/base_integration.py
+++ integration/base_integration.py
@@ -3,7 +3,6 @@ from integration.config import INTEGRATION_CLASSES
 
 async def base_integration(intent=None, entities=None):
     integration = INTEGRATION_CLASSES.get(intent)
-    if integration:
-        integration_init = integration(entities)
-        integration_response = await integration_init.main()
-        return integration_response
+    integration_init = integration(entities)
+    integration_response = await integration_init.main()
+    return integration_response
